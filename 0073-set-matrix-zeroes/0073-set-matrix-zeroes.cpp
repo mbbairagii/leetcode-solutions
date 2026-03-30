d52abd7a8003efb396d8806b9951470ac77cd39a
Mohini <mbbairagii@gmail.com>
2026-03-30 11:29:16 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -3,19 +3,22 @@ public:
     void setZeroes(vector<vector<int>>& matrix) {
         int m=matrix.size();
         int n=matrix[0].size();
-        vector<vector<int>> copy=matrix;
+        vector<int> row(m,0);
+        vector<int> col(n,0);
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
                 if(matrix[i][j]==0){
-                    for(int col=0;col<n;col++){
-                        copy[i][col]=0;
-                    }
-                    for(int row=0;row<m;row++){
-                        copy[row][j]=0;
-                    }
+                    row[i]=1;
+                    col[j]=1;
                 }
             }
         }
-        matrix=copy;
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(row[i]==1 || col[j]==1){
+                    matrix[i][j]=0;
+                }            
+            }
+        }
     }
 };
