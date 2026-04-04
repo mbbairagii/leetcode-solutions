5f51d9636db295056605c8f1cc81a00f260333d6
Mohini <mbbairagii@gmail.com>
2026-04-04 09:14:16 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.cpp
+++ 0229-majority-element-ii/0229-majority-element-ii.cpp
@@ -2,27 +2,38 @@ class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
         int n=nums.size();
-        vector<int> result;
-        for(int i=0;i<n;i++){
-            int count=0;
-            for(int j=0;j<n;j++){
-                if(nums[i]==nums[j]){
-                    count++;
-                }
+        int c1=0;
+        int c2=0;
+        int candidate1=0;
+        int candidate2=1;
+        for(int num: nums){
+            if(num==candidate1) c1++;
+            else if(num==candidate2) c2++;
+            else if(c1==0){
+                candidate1=num;
+                c1=1;
             }
-            if(count>n/3){
-                bool alreadypresentinresult=false;
-                for(int x:result){
-                    if(x==nums[i]){
-                        alreadypresentinresult=true;
-                        break;
-                    }
-                }
-                if(!alreadypresentinresult){
-                    result.push_back(nums[i]);
-                }
+            else if(c2==0){
+                candidate2=num;
+                c2=1;
             }
+            else{
+                c1--;
+                c2--;
+            }
+
         }
+
+        c1=0;
+        c2=0;
+        for(int num: nums){
+            if(num==candidate1) c1++;
+            else if(num==candidate2) c2++;
+        }
+
+        vector<int> result;
+        if(c1>n/3) result.push_back(candidate1);
+        if(c2>n/3) result.push_back(candidate2);
         return result;
     }
 };
