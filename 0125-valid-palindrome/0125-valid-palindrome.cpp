f7f461c95e3f1cb4712d3aa7d10325e88c2a68aa
Mohini <mbbairagii@gmail.com>
2026-08-05 20:47:20 +0530

    Time: 0 ms (100%), Space: 10 MB (36.6%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.cpp
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -1,17 +1,20 @@
 class Solution {
 public:
-    bool helper(string &s, int i, int j) {
-        if (i >= j) return true;
-
-        if (!isalnum(s[i])) return helper(s, i+1, j);
-        if (!isalnum(s[j])) return helper(s, i, j-1);
-
-        if (tolower(s[i]) != tolower(s[j])) return false;
-
-        return helper(s, i+1, j-1);
-    }
-
     bool isPalindrome(string s) {
-        return helper(s, 0, s.size()-1);
+        int left=0;
+        int right=s.size()-1;
+        while(left<right){
+            while(left<right && !isalnum(s[left])) left++;
+            while(left<right && !isalnum(s[right])) right--;
+
+            if(tolower(s[right]) != tolower(s[left])){
+                return false;
+            }
+
+            left++;
+            right--;
+        }
+
+        return true;
     }
 };
