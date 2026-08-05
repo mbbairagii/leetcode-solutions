839ce7eadc1392136bce9426a71282c135781851
Mohini <mbbairagii@gmail.com>
2026-08-05 19:46:31 +0530

    Time: 2517 ms (8.25%), Space: 34.4 MB (87.89%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
@@ -6,7 +6,7 @@ public:
         for(int i=0;i<n;i++){
             int sum=0;
             for(int j=i;j<n;j++){
-                sum = sum +nums[j];
+                sum=sum+nums[j];
                 if(sum==k){
                     count++;
                 }
