ab278f4e0264a5b5b80b19fdb13228a86a775231
Mohini <mbbairagii@gmail.com>
2026-09-19 01:38:20 +0530

    Time: 11 ms (21.72%), Space: 20.1 MB (25.35%) - LeetHub

---
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+        vector<vector<int>> adj(numCourses);
+        vector<int> indegree(numCourses,0);
+
+        for(auto pre: prerequisites){
+            int u=pre[0];
+            int v=pre[1];
+
+            adj[u].push_back(v);
+            indegree[v]++;
+        }
+
+        queue<int> q;
+        for(int i=0;i<numCourses;i++){
+            if(indegree[i]==0){
+                q.push(i);
+            }
+        }
+
+        int completed=0;
+        while(!q.empty()){
+            int node=q.front();
+            q.pop();
+            completed++;
+            for(int neighbour: adj[node]){
+                indegree[neighbour]--;
+                if(indegree[neighbour]==0){
+                    q.push(neighbour);
+                }
+            }
+        }
+
+        return completed==numCourses;
+    }
+};
